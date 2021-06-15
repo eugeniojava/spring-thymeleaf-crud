0d180364bf14cb75efe0439e3bffd72eb45b8ba3
Eugênio Moreira <eugeniojava@outlook.com>
2021-06-15 10:18:46 -0300

    first commit

---
+++ src/main/java/com/eugeniojava/springthymeleafcrud/SpringThymeleafCrudApplication.java
@@ -0,0 +1,12 @@
+package com.eugeniojava.springthymeleafcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafCrudApplication.class, args);
+    }
+}
---
+++ src/test/java/com/eugeniojava/springthymeleafcrud/SpringThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugeniojava.springthymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
